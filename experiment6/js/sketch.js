72840abaa2d1aaf4454a0c4cf11d5ccdbda48a51
remogarc <remogarc@ucsc.edu>
2024-02-20 16:06:02 -0800

    adding 6 and setup 7

--- experiment6/js/sketch.js
+++ experiment6/js/sketch.js
@@ -1,6 +1,6 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+// sketch.js - Grammars & Text Art
+// Author: Reese Garcia
+// Date: 2/19/24
 
 // Here is how you might set up an OOP p5.js project
 // Note that p5.js looks for a file called sketch.js
@@ -43,25 +43,90 @@ function setup() {
     var centerVert = windowHeight / 2;
 }
 
-// draw() function is called repeatedly, it's the main animation loop
-function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
+p5.disableFriendlyErrors = true;
+let font1;
+let radius = 200;
+let tubeRadius = 100;
+let textTexture;
+let hu = 0;
+let indexWord = 0;
+let canvas;
+let words = ['^ /\\ ^ /\\ ^ /\\ ^ /\\ ^ /\\ ^ /\\ ^ /\\ ^ /\\ ^ /\\ ^ /\\ ^ /\\ ^ /\\ ^ /\\ ^ /\\ ^ /\\ ^'];
 
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
+// canvas = createCanvas(1112, 834,WEBGL);
+
+function setup() {
+    // place our canvas, making it fit our container
+    canvasContainer = $("#canvas-container");
+    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height(), WEBGL);
+    canvas.parent("canvas-container");
+    // resize canvas is the page is resized
+    $(window).resize(function() {
+        console.log("Resizing...");
+        resizeCanvas(canvasContainer.width(), canvasContainer.height());
+    });
+	// Create Texture
+  textTexture = createGraphics(2*PI*radius,2*PI*400);
+	textTexture.clear()
+	textTexture.colorMode(HSB, 255);
+  textTexture.fill(255);
+	textTexture.textFont('arial');
+  textTexture.textSize(51);
+	textTexture.translate(0,words.length*25);
+
+	noStroke();
+  background(25, 0, 70);
 }
 
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
+function draw() {
+	
+	orbitControl();
+
+	// Add text to the texture
+	//let wave = (sin(frameCount * 0.005 + (0.005) * 0.005) * 1);
+	textTexture.clear();
+	
+	for(let i = 0; i <=31; i++){
+    textTexture.fill((hu + i*8)%255, 200, 255);
+		textTexture.text(words[i%3], 0, i*70);
+	}
+	
+	// Create Solid
+	push();
+	rotateZ(radians(-90));
+	rotateX(radians(90));
+	rotateY(PI/2 + frameCount*0.008);
+	push();
+	texture(textTexture);
+	cylinder(radius, 2500, 40, 1, true, false);
+
+	pop();
+	pop();
+  
+    push();
+	rotateZ(radians(-90));
+	rotateX(radians(-90));
+	rotateY(PI/2 + frameCount*0.008);
+	push();
+	texture(textTexture);
+	cone(radius, 1750, 40, 1, false);
+
+	pop();
+	pop();
+	hu++;
+	
+	
+}
+
+function keyTyped(){
+  if (key === 'c'){
+    clear();
+    background(25, 0, 70);
+  }
+  if (key === 'x'){
+    textTexture.colorMode(RGB, 255);
+  }
+  if (key === 'z'){
+    textTexture.colorMode(HSB, 255);
+  }
 }
